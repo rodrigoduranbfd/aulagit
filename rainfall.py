1738c4c0070a509a429d9e226d3c403099196339
rodrigoduran <rodrigo.duran@ifms.edu.br>
2025-09-07 19:52:05 -0300

    adicionada função para guardar divisão por zero

--- rainfall.py
+++ rainfall.py
@@ -3,6 +3,9 @@ SENTINELA = -99999
 def filtraDados (lista):
     return filter(lambda x: x >=0, lista)
 
+def guardaZero (lista):
+    return len(lista) > 0
+
 def coletarDados ():
     lista = [] 
     entrada = input("Digite um número ou -99999 para terminar: ") 
@@ -12,7 +15,7 @@ def coletarDados ():
     return lista   
 
 def mediaChuvas(lista): 
-    if len(lista) > 0:
+    if (guardaZero(lista)):
         return sum(lista)/len(lista)
     else :
         return -1
